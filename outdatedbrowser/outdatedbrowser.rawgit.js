6ec22ab16a6699af93cce7b253e21e7bc44f6102
crucify <shj@xenosi.de>
2015-06-22 18:24:08 +0900

    use rawgit CDN

---
+++ outdatedbrowser/outdatedbrowser.rawgit.js
@@ -0,0 +1,218 @@
+/*!--------------------------------------------------------------------
+ JAVASCRIPT "Outdated Browser"
+ Version:    1.1.0 - 2014
+ author:     Burocratik
+ website:    http://www.burocratik.com
+ * @preserve
+
+ Modified by Song Hyo Jin (shj at xenosi.de) for use CDN and Auto detect Language
+ -----------------------------------------------------------------------*/
+var outdatedBrowser = function(options) {
+
+	//Variable definition (before ajax)
+	var outdated = document.createElement('div');
+	outdated.id = 'outdated';
+	document.body.appendChild(outdated);
+
+	// Default settings
+	this.defaultOpts = {
+		bgColor: '#f25648',
+		color: '#ffffff',
+		lowerThan: 'transform',
+		language: navigator.language ? navigator.language.toLowerCase().replace(/^([a-z]{2}).*$/, '$1') : 'en'
+	}
+
+	if (options) {
+		//assign css3 property to IE browser version
+		if(options.lowerThan == 'IE8' || options.lowerThan == 'borderSpacing') {
+			options.lowerThan = 'borderSpacing';
+		} else if (options.lowerThan == 'IE9' || options.lowerThan == 'boxShadow') {
+			options.lowerThan = 'boxShadow';
+		} else if (options.lowerThan == 'IE10' || options.lowerThan == 'transform' || options.lowerThan == '' || typeof options.lowerThan === "undefined") {
+			options.lowerThan = 'transform';
+		} else if (options.lowerThan == 'IE11' || options.lowerThan == 'borderImage') {
+			options.lowerThan = 'borderImage';
+		}
+		//all properties
+		if(options.bgColor) this.defaultOpts.bgColor = options.bgColor;
+		if(options.color) this.defaultOpts.color = options.color;
+		this.defaultOpts.lowerThan = options.lowerThan;
+		if(options.language) this.defaultOpts.language = options.language;
+
+		bkgColor = this.defaultOpts.bgColor;
+		txtColor = this.defaultOpts.color;
+		cssProp = this.defaultOpts.lowerThan;
+		language = this.defaultOpts.language;
+	} else {
+		bkgColor = this.defaultOpts.bgColor;
+		txtColor = this.defaultOpts.color;
+		cssProp = this.defaultOpts.lowerThan;
+		language = this.defaultOpts.language;
+	};//end if options
+
+
+	//Define opacity and fadeIn/fadeOut functions
+	var done = true;
+
+	function function_opacity(opacity_value) {
+		outdated.style.opacity = opacity_value / 100;
+		outdated.style.filter = 'alpha(opacity=' + opacity_value + ')';
+	}
+
+	// function function_fade_out(opacity_value) {
+	//     function_opacity(opacity_value);
+	//     if (opacity_value == 1) {
+	//         outdated.style.display = 'none';
+	//         done = true;
+	//     }
+	// }
+
+	function function_fade_in(opacity_value) {
+		function_opacity(opacity_value);
+		if (opacity_value == 1) {
+			outdated.style.display = 'block';
+		}
+		if (opacity_value == 100) {
+			done = true;
+		}
+	}
+
+	//check if element has a particular class
+	// function hasClass(element, cls) {
+	//     return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+	// }
+
+	var supports = (function() {
+		var vendors = 'Khtml Ms O Moz Webkit'.split(' '),
+			len = vendors.length;
+
+		return function(prop) {
+			if ( prop in outdated.style ) return true;
+
+			prop = prop.replace(/^[a-z]/, function(val) {
+				return val.toUpperCase();
+			});
+
+			while(len--) {
+				if ( vendors[len] + prop in div.style ) {
+					return true;
+				}
+			}
+			return false;
+		};
+	})();
+
+	//if browser does not supports css3 property (transform=default), if does > exit all this
+	if ( !supports(''+ cssProp +'') ) {
+		if (done && outdated.style.opacity !== '1') {
+			done = false;
+			for (var i = 1; i <= 100; i++) {
+				setTimeout((function (x) {
+					return function () {
+						function_fade_in(x);
+					};
+				})(i), i * 8);
+			}
+		}
+	}else{
+		return;
+	};//end if
+
+	//Check AJAX Options: if languagePath == '' > use no Ajax way, html is needed inside <div id="outdated">
+	//if( languagePath === ' ' || languagePath.length == 0 ){
+	//    startStylesAndEvents();
+	//}else{
+	var csstag = document.createElement('link');
+	csstag.rel = 'stylesheet';
+	csstag.href = '//cdn.rawgit.com/burocratik/outdated-browser/develop/outdatedbrowser/outdatedbrowser.min.css';
+	grabFile(language);
+	//}
+
+	//events and colors
+	function startStylesAndEvents(){
+		var btnClose = document.getElementById("btnCloseUpdateBrowser");
+		var btnUpdate = document.getElementById("btnUpdateBrowser");
+
+		//check settings attributes
+		outdated.style.backgroundColor = bkgColor;
+		//way too hard to put !important on IE6
+		outdated.style.color = txtColor;
+		outdated.children[0].style.color = txtColor;
+		outdated.children[1].style.color = txtColor;
+
+		//check settings attributes
+		btnUpdate.style.color = txtColor;
+		// btnUpdate.style.borderColor = txtColor;
+		if (btnUpdate.style.borderColor) btnUpdate.style.borderColor = txtColor;
+		btnClose.style.color = txtColor;
+
+		//close button
+		btnClose.onmousedown = function() {
+			outdated.style.display = 'none';
+			return false;
+		};
+
+		//Override the update button color to match the background color
+		btnUpdate.onmouseover = function() {
+			this.style.color = bkgColor;
+			this.style.backgroundColor = txtColor;
+		};
+		btnUpdate.onmouseout = function() {
+			this.style.color = txtColor;
+			this.style.backgroundColor = bkgColor;
+		};
+	}//end styles and events
+
+
+	// IF AJAX with request ERROR > insert english default
+	var ajaxEnglishDefault = '<h6>Your browser is out-of-date!</h6>'
+		+ '<p>Update your browser to view this website correctly. <a id="btnUpdateBrowser" href="http://outdatedbrowser.com/">Update my browser now </a></p>'
+		+ '<p class="last"><a href="#" id="btnCloseUpdateBrowser" title="Close">&times;</a></p>';
+
+
+	//** AJAX FUNCTIONS - Bulletproof Ajax by Jeremy Keith **
+	function getHTTPObject() {
+		var xhr = false;
+		if (window.XMLHttpRequest) {
+			xhr = new XMLHttpRequest();
+		} else if (window.ActiveXObject) {
+			try {
+				xhr = new ActiveXObject("Msxml2.XMLHTTP");
+			} catch(e) {
+				try {
+					xhr = new ActiveXObject("Microsoft.XMLHTTP");
+				} catch(e) {
+					xhr = false;
+				}
+			}
+		}
+		return xhr;
+	};//end function
+
+	function grabFile(language) {
+		var request = getHTTPObject();
+		if (request) {
+			request.onreadystatechange = function() {
+				displayResponse(request);
+			};
+			request.open("GET", '//cdn.rawgit.com/burocratik/outdated-browser/develop/outdatedbrowser/lang/' + language + '.html', true);
+			request.send(null);
+		}
+		return false;
+	};//end grabFile
+
+	function displayResponse(request) {
+		var insertContentHere = document.getElementById("outdated");
+		if (request.readyState == 4) {
+			if (request.status == 200 || request.status == 304) {
+				insertContentHere.innerHTML = request.responseText;
+			}else{
+				insertContentHere.innerHTML = ajaxEnglishDefault;
+			}
+			startStylesAndEvents();
+		}
+		return false;
+	};//end displayResponse
+
+////////END of outdatedBrowser function
+};
